952b117a4e6558806f9134e4adffacae306a81e4
Fabio Rueda <avances123@gmail.com>
2014-04-18 14:24:02 +0200

    creo un shape con los puntos

--- wrf.py
+++ wrf.py
@@ -1,6 +1,8 @@
-from osgeo import gdal,gdalconst
+from osgeo import gdal,gdalconst,ogr,osr
 import sys
 import numpy as np
+
+
 
 
 
@@ -11,9 +13,34 @@ def get_coords(filepath):
     raster = gdal.Open(filepath,gdalconst.GA_ReadOnly)
     matriz = raster.ReadAsArray() #(262, 695)
     upper_left_x, x_size, x_rotation, upper_left_y, y_rotation, y_size = raster.GetGeoTransform()
-    y_index, x_index = np.nonzero(matriz > 254)
+    y_index, x_index = np.nonzero(matriz < 255)
     x_coords = x_index * x_size + upper_left_x + (x_size / 2) #add half the cell size
     y_coords = y_index * y_size + upper_left_y + (y_size / 2) #to centre the point
-    print x_coords,y_coords
+    
+    
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    shapeData = driver.CreateDataSource('ogr_pts')
+    srs = osr.SpatialReference()
+    srs.ImportFromWkt(raster.GetProjection())
+    layer = shapeData.CreateLayer('ogr_pts', srs, ogr.wkbPoint)
+    layerDefinition = layer.GetLayerDefn()
+
+
+    indexes = np.column_stack((x_index,y_index))
+    coords = np.column_stack((x_coords,y_coords))
+    i = 0
+    for x,y in coords:
+        print x,y
+        point = ogr.Geometry(ogr.wkbPoint)
+        point.SetPoint(0, x, y)
+
+        feature = ogr.Feature(layerDefinition)
+        feature.SetGeometry(point)
+        feature.SetFID(i)
+
+        layer.CreateFeature(feature)
+
+        i += 1
+
 
 get_coords(sys.argv[1])
